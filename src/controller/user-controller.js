9af4aacd4fd951e28c14d0588b56fad01d49e9e5
Hilbert <blitherium@gmail.com>
2022-06-07 16:24:37 +0800

    Innit new main branch

---
+++ src/controller/user-controller.js
@@ -0,0 +1,39 @@
+const { getAuth } = require("firebase-admin/auth");
+
+exports.GetUser = async (req, res) => {
+  await getAuth()
+    .getUser(req.params.uid)
+    .then((userRecord) => {
+      if (typeof userRecord === "undefined") {
+        res.status(404).json({
+          message: "User not found!",
+        });
+      } else {
+        res.status(200).json(userRecord);
+      }
+    })
+    .catch((error) => {
+      res.json(error);
+    });
+};
+
+exports.UpdateUser = async (req, res) => {
+  const { email, phoneNumber, password, displayName, photoURL } = req.body;
+  await getAuth()
+    .updateUser(req.params.uid, {
+      email: email,
+      phoneNumber: phoneNumber,
+      password: password,
+      displayName: displayName,
+      photoURL: photoURL,
+    })
+    .then((userRecord) => {
+      res.status(200).json({
+        'message': 'Successfully updated user', 
+        'userRecord': userRecord
+      });
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
+}
