9e8aca58f1dc7a283efe7b279ea6ec2cb0f73806
Hilbert <88700493+hilberthp@users.noreply.github.com>
2022-06-12 13:18:56 +0800

    Remove input recommender since it's from ML side

--- src/controllers/recommender-controller.js
+++ src/controllers/recommender-controller.js
@@ -1,41 +1,19 @@
 import { db } from "../../app.js";
 
 export async function AddRecommender(req, res, next) {
-  const { jenisUsaha, kategoriUsaha, rekomendasi } = req.body;
-  const document = db.collection("recommender").doc();
+  const { punya_usaha, bidang_keahlian, hobi, modal_usaha, nama_usaha } = req.body;
+  const document = db.collection("users").doc(req.uid);
   await document
-    .set({
-      uid: req.uid,
-      jenisUsaha: jenisUsaha,
-      kategoriUsaha: kategoriUsaha,
-      rekomendasi: rekomendasi,
-    })
-    .then(() => {
-      res.status(200).json({
-        code: 200,
-        message: "Successfully added recommender with ID: " + document.id,
-      });
-    })
-    .catch((error) => {
-      res.status(500).json({
-        code: 500,
-        error: error,
-      });
-    });
-}
-
-export async function AddInputRecommender(req, res, next) { 
-  const { punyaUsaha, bidangKeahlian, hobi, modalUsaha, namaUsaha } = req.body;
-  const document = db.collection("inputRecommender").doc();
-  await document
-    .set({
-      uid: req.uid,
-      punyaUsaha: punyaUsaha,
-      bidangKeahlian: bidangKeahlian,
-      hobi: hobi,
-      modalUsaha: modalUsaha,
-      namaUsaha: namaUsaha,
-    })
+    .set(
+      {
+        punya_usaha: punya_usaha,
+        bidang_keahlian: bidang_keahlian,
+        hobi: hobi,
+        modal_usaha: modal_usaha,
+        nama_usaha: nama_usaha,
+      }, 
+      { merge: true }
+    )
     .then(() => {
       res.status(200).json({
         code: 200,
@@ -51,39 +29,15 @@ export async function AddInputRecommender(req, res, next) {
 }
 
 export async function GetRecommender(req, res, next) {
-  const document = db.collection("recommender").where("uid", "==", req.uid);
+  const document = db.collection("users").doc(req.uid);
   await document
     .get()
-    .then((snapshot) => {
-      if (snapshot.docs.length > 0) {
+    .then((doc) => {
+      console.log(doc.exists)
+      if (doc.exists) {
         res.status(200).json({
           code: 200,
-          snapshot: snapshot.docs[0].data()
-        });
-      } else {
-        res.status(404).json({
-          code: 404,
-          message: "Document does not exist!",
-        });
-      }
-    })
-    .catch((error) => {
-      res.status(500).json({
-        code: 500,
-        error: error,
-      });
-    });
-}
-
-export async function GetInputRecommender(req, res, next) {
-  const document = db.collection("inputRecommender").where("uid", "==", req.uid);
-  await document
-    .get()
-    .then((snapshot) => {
-      if (snapshot.docs.length > 0) {
-        res.status(200).json({
-          code: 200,
-          snapshot: snapshot.docs[0].data()
+          doc: doc.data(),
         });
       } else {
         res.status(404).json({
--- src/routes/recommender-routes.js
+++ src/routes/recommender-routes.js
@@ -2,16 +2,12 @@ import app from "express";
 import Middleware from "../middleware/middleware.js";
 import {
   AddRecommender,
-  AddInputRecommender,
-  GetRecommender,
-  GetInputRecommender
+  GetRecommender
 } from "../controllers/recommender-controller.js";
 
 const router = app.Router();
 
 router.post("/", Middleware, AddRecommender);
-router.post("/input", Middleware, AddInputRecommender);
 router.get("/", Middleware, GetRecommender);
-router.get("/input", Middleware, GetInputRecommender);
 
 export { router as RecommenderRoutes };
